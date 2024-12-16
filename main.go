11defd34d2ed3939d809ec1b3b837cd42622c4bb
shin1244 <tlswnsrb1357@gmail.com>
2024-12-16 23:07:16 +0900

    링크드 리스트 append, push 기능 추가

--- main.go
+++ main.go
@@ -2,11 +2,30 @@ package main
 
 import "fmt"
 
-// type Node[T any] struct {
-// 	next *Node
-// 	val  T
-// }
+type Node[T any] struct {
+	next *Node[T]
+	val  T
+}
+
+func Append[T any](node *Node[T], next *Node[T]) *Node[T] {
+	node.next = next
+	return next
+}
+
+func Push[T any](node *Node[T], next *Node[T]) {
+	next.next = node.next
+	node.next = next
+}
 
 func main() {
-	fmt.Println("하이요")
+	root := &Node[int]{nil, 10}
+	tail := root
+
+	for i := 1; i < 4; i++ {
+		tail = Append(tail, &Node[int]{nil, 10 + i*10})
+	}
+
+	for n := root; n != nil; n = n.next {
+		fmt.Println(n.val)
+	}
 }
