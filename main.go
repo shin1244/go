16a5eedfdf1c770c8e8f88882a9faf7504fee3f0
shin1244 <tlswnsrb1357@gmail.com>
2024-12-16 22:22:01 +0900

    ok

--- main.go
+++ main.go
@@ -2,10 +2,10 @@ package main
 
 import "fmt"
 
-type Node[T any] struct {
-	next *Node
-	val  T
-}
+// type Node[T any] struct {
+// 	next *Node
+// 	val  T
+// }
 
 func main() {
 	fmt.Println("하이요")
