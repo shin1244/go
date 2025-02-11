5bbac8ae4efc33fbae707372977cbd101fd4b771
shin1244 <tlswnsrb1357@gmail.com>
2025-02-11 16:54:37 +0900

    함수 리턴턴

--- func/main.go
+++ func/main.go
@@ -1,6 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
+
+type opfn[T ~int | ~float64] func(i ...T) T
 
 func sum[T ~int | ~float64](i ...T) T {
 	var sum T
@@ -10,8 +15,42 @@ func sum[T ~int | ~float64](i ...T) T {
 	return sum
 }
 
+func mul[T ~int | ~float64](i ...T) T {
+	var mul T
+	mul = 1
+	for _, v := range i {
+		mul *= v
+	}
+	return mul
+}
+
+func operator[T ~int | ~float64](op string) opfn[T] {
+	if op == "+" {
+		return sum[T]
+	} else if op == "*" {
+		return mul[T]
+	}
+	return nil
+}
+
+func create() {
+	f, err := os.Create("test.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	defer fmt.Println("파일 닫기")
+
+	fmt.Fprintln(f, "Hello, World!")
+}
+
 func main() {
 	fmt.Println(sum(1, 2, 3, 4, 5))
 	fmt.Println(sum(100, 200))
 	fmt.Println(sum(0.3, 0.7))
+	create()
+	op := operator[int]("+")
+	fmt.Println(op(1, 2, 3, 4, 5))
+	op = operator[int]("*")
+	fmt.Println(op(1, 2))
 }
